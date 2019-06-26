a562b7cd1ae295fb07db8a2a32b309250aaa6253
vanderson lima <vanderson.lima1@gmail.com>
2019-06-25 21:23:36 -0300

    teste webpack e babel

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ app.get('/',function(req,res){
   console.log(__dirname);
   //api = new Dotenv().API_SPORTMONKS;  
   //console.log(api);
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/src/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
 //app.use('/content', express.static('public'))
-app.use('/public', express.static('public'))
+app.use('/public', express.static('src/public'))
 app.listen(8081);
 console.log("Server running at Port 8081");
