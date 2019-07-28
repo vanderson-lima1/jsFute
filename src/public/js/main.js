467502aa54cd1b8f23a4fc205503c7b7825520c7
vanderson <vanderson.lima1@gmail.com>
2019-07-28 11:06:09 -0300

    tentando arrumar chamada ajax

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,14 +1,20 @@
 import {apiSportMonks} from '../js/sportMonks.js';
-let data = apiSportMonks('/leagues','');
 
-if(data != null){
-  let leagues = [];
-  leagues = JSON.stringify(data);
-  leagues.forEach(  item => {
+//let documento = $(document);
+let datas =  apiSportMonks(callback,'/leagues','');
+if(datas != null){
+  //let leagues = [];
+  let leagues = JSON.stringify(datas);
+  console.log("data main: "+ datas);
+  //leagues = JSON.stringify(datas["datas"]);
+  console.log("leagues main: " + leagues);
+  //$(response).each(function(index, item)
+  //leagues.forEach(  item => {
+  $.each(leagues, function (index) {
     //let card = item[index];
     //$('#cardTest').append(card);  
-    console.log("/league: " + item);
-  });
+    console.log("league: " + this[index] + " " + index);
+  })
 }
 else {
-  console.log("teste: " + data != null); };
+  console.log("data nulo aq: " + datas); };
--- src/public/js/sportMonks.js
+++ src/public/js/sportMonks.js
@@ -17,7 +17,8 @@ axios({
   .catch(erro => {
     console.log(error)
   });*/
-  export async function apiSportMonks (action, params) { 
+  //export async function apiSportMonks (action, params) { 
+  export function apiSportMonks (callback,action, params) { 
   let urlSportMonks = 'https://soccer.sportmonks.com/api/v2.0';
   let urlAction = action;
 
@@ -30,41 +31,50 @@ axios({
     urlParm = params;
   };
 
-  let urlTokenSportMonks = '?api_token=EXvm21l8AzmE2K8LmbexE7PRr4OqforQDO8j1HiacYXS2uG4OqQLgQrVifV0';
+  let urlTokenSportMonks = '?api_token=hvKskSL1XiEf3glA4QceYiPeFNwDpaOJzp8iitIpikDo3GEioubYVs4DWsEM';
+  
 
-  await $.ajax({
-    url: urlSportMonks+urlAction+urlParm+urlTokenSportMonks,
-    dataType: 'json',
-    type: 'GET',
-    success: (response) => {
-      
-      console.log(response);
-      return response;
-      // order UF.
-      /*
-      response.sort(function(a, b){
-          if ( a.sigla < b.sigla ){
-              return -1;
-          }
-          if ( a.sigla > b.sigla ){
-              return 1;
-          }
-          return 0;
-      })*/
+//  documento.ready( function() {
+//await $.ajax({
+    $.ajax({
+      url: urlSportMonks+urlAction+urlParm+urlTokenSportMonks,
+      dataType: 'json',
+      type: 'GET',
+      success: (response) => {
+        
+        console.log("response: "+response);
+        let datas = JSON.stringify(response);
+        //return datas;
+        callback();
+        return datas;
+        // order UF.
+        /*
+        response.sort(function(a, b){
+            if ( a.sigla < b.sigla ){
+                return -1;
+            }
+            if ( a.sigla > b.sigla ){
+                return 1;
+            }
+            return 0;
+        })*/
 
-      // Save to localStorage
-      //localStorage.setItem('listaUF', JSON.stringify(response));
+        // Save to localStorage
+        //localStorage.setItem('listaUF', JSON.stringify(response));
 
-      // build options list
-      /*
-      $(response).each(function(index, item){
-          var option = $(`<option value="${item.sigla}">${item.sigla}</option>`);
-          $('#addressUF').append(option);
-      })*/
-    },
-    error: (response) => {
-        console.error('Error', response);
-        return null;
-    }
-  });
-};
+        // build options list
+        /*
+        $(response).each(function(index, item){
+            var option = $(`<option value="${item.sigla}">${item.sigla}</option>`);
+            $('#addressUF').append(option);
+        })*/
+      },
+      error: (response) => {
+          console.error('Error', response);
+          //return null;
+          callback();
+          return datas;
+      }
+    });
+  //});
+  };
